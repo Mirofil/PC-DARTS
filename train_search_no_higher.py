7393cb998650c109e2d4e5149f0163af5bd72c7f
Mirofil <kawgawr@gmail.com>
2021-07-15 00:34:52 +0100

    Bugs

--- train_search_no_higher.py
+++ train_search_no_higher.py
@@ -231,6 +231,7 @@ def main():
   api = load_nb301()
 
   if os.path.exists(Path(args.save) / "checkpoint.pt"):
+    print(f"Checkpoint found")
     checkpoint = torch.load(Path(args.save) / "checkpoint.pt")
     optimizer.load_state_dict(checkpoint["w_optimizer"])
     architect.optimizer.load_state_dict(checkpoint["a_optimizer"])
@@ -244,7 +245,7 @@ def main():
     start_epoch=0
     all_logs=[]
 
-  for epoch in tqdm(range(args.epochs), desc = "Iterating over epochs"):
+  for epoch in tqdm(range(start_epoch, args.epochs), desc = "Iterating over epochs"):
     scheduler.step()
     lr = scheduler.get_lr()[0]
     logging.info('epoch %d lr %e', epoch, lr)
